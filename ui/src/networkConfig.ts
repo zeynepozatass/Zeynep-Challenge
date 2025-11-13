6458cf06dbdc772a82e6dea7c27e64a6c3c75458
Zeynep <zeynepgulozatas732@gmail.com>
2025-11-13 19:59:35 +0300

    Package ID eklendi

--- ui/src/networkConfig.ts
+++ ui/src/networkConfig.ts
@@ -1,20 +1,8 @@
 import { getFullnodeUrl } from "@mysten/sui/client";
 import { createNetworkConfig } from "@mysten/dapp-kit";
 
-/**
- * Package ID retrieved from transaction summary, after runnning `sui client publish`
- *
- * Example:
- * ```bash
- *  Published Objects:
- * ┌──
- * │ PackageID: 0xdbd32a4b9802fab3bca9f7c7cb339d9a88d3b271581280cb83df487ce87a65e6
- * │ Version: 1
- * │ Digest: bn8Vs7TgMzhyPN4GtjDdjTfufX67dErp4926bQeCSFr
- * │ Modules: arena, hero, marketplace
- * └──
- */
-const PACKAGE_ID = "";
+
+const PACKAGE_ID = "0x9fbe8d4a7b9fb3cb19f2a18171badb922b2bc93ade85deb5b50e1689e8a6885d";
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
